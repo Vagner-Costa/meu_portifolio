ba9002acd5dbd33c33ec8053545f64c4d3cb0e05
vagner <seu.email@customer.com.br>
2023-05-05 16:58:37 -0300

    Atualizando

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath : process.env.NODE_ENV === 'production' ? '/portifolio/' : '/'
+  publicPath : process.env.NODE_ENV === 'production' ? '/meu_portifolio/' : '/'
 }
 
 
