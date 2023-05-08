e1bccd5ec7d0f73f90770f0b09869cb75851e98b
vagner <seu.email@customer.com.br>
2023-05-08 18:38:59 -0300

    comitando

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath : process.env.NODE_ENV === 'production' ? './meu_portifolio/' : '/'
+  publicPath : process.env.NODE_ENV === 'production' ? '/meu_portifolio/' : '/'
 }
